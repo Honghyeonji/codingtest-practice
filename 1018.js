a7477ad8ac9cba361b07c1a710d92267bfdb5140
Honghyeonji <hyeonji8436@gmail.com>
2024-07-16 17:52:53 +0900

    Create 1018.js

---
+++ 1018.js
@@ -0,0 +1,46 @@
+const fs = require("fs");
+const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+
+const chess_ver1 = [
+  ["W", "B", "W", "B", "W", "B", "W", "B"],
+  ["B", "W", "B", "W", "B", "W", "B", "W"],
+  ["W", "B", "W", "B", "W", "B", "W", "B"],
+  ["B", "W", "B", "W", "B", "W", "B", "W"],
+  ["W", "B", "W", "B", "W", "B", "W", "B"],
+  ["B", "W", "B", "W", "B", "W", "B", "W"],
+  ["W", "B", "W", "B", "W", "B", "W", "B"],
+  ["B", "W", "B", "W", "B", "W", "B", "W"],
+];
+
+const chess_ver2 = [
+  ["B", "W", "B", "W", "B", "W", "B", "W"],
+  ["W", "B", "W", "B", "W", "B", "W", "B"],
+  ["B", "W", "B", "W", "B", "W", "B", "W"],
+  ["W", "B", "W", "B", "W", "B", "W", "B"],
+  ["B", "W", "B", "W", "B", "W", "B", "W"],
+  ["W", "B", "W", "B", "W", "B", "W", "B"],
+  ["B", "W", "B", "W", "B", "W", "B", "W"],
+  ["W", "B", "W", "B", "W", "B", "W", "B"],
+];
+
+const [row, col] = input[0].split(" ").map(Number);
+const arr = input.slice(1).map((line) => line.split(""));
+
+let min = 10000;
+
+for (let i = 0; i <= row - 8; i++) {
+  for (let j = 0; j <= col - 8; j++) {
+    let ver1_count = 0;
+    let ver2_count = 0;
+    for (let v = i; v < i + 8; v++) {
+      for (let w = j; w < j + 8; w++) {
+        if (chess_ver1[v - i][w - j] !== arr[v][w]) ver1_count++;
+        if (chess_ver2[v - i][w - j] !== arr[v][w]) ver2_count++;
+      }
+    }
+    if (ver1_count < min) min = ver1_count;
+    if (ver2_count < min) min = ver2_count;
+  }
+}
+
+console.log(min);
